101a6bd81ba67bc20738316924c76fdd68be736e
NickDane2021 <78652512+NickDane2021@users.noreply.github.com>
2021-06-28 19:26:24 +0400

    RobuxGenerator

---
+++ test1.js
@@ -0,0 +1,15 @@
+
+  
+  var button = document.getElementById("btn")
+  
+  
+  
+  button.addEventListener('click', function(){
+  
+    prompt("chawere sheni robloxis saxeli qvevit")
+   
+    prompt("chawere ramdeni robuxi ginda")
+  
+    alert('robuxebi charicxulia gilocav')
+  
+  })
